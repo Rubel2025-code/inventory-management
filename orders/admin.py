86a934b6d68c6b317a709062240958d505f2444c
Rubel <miahrubel768.cse@gmail.com>
2025-07-26 03:48:58 +0600

    register cart,cartitem,and order models in admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Cart, CartItem, Order
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['cart', 'product', 'quantity']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'transaction_number', 'status', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['transaction_number', 'user__username']
+    filter_horizontal = ['items']
