965ae92202370ca15c53deb82b87cd0e06548090
Rubel <miahrubel768.cse@gmail.com>
2025-08-10 01:34:53 +0600

    add picture

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from inventory import views as inventory_views
 from orders import views as order_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
