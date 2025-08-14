2da0e2bd2454606ecf19b88c6697aae016c295c7
Rubel <miahrubel768.cse@gmail.com>
2025-08-15 03:04:32 +0600

    add email

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
@@ -68,7 +68,7 @@ def export_csv(request):
     return response
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
