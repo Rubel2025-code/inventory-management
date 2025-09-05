13272969b91dd9dc91e3464f44fda9174fb00b4d
Rubel <miahrubel768.cse@gmail.com>
2025-09-05 10:29:38 +0600

    add supplier info

--- inventory/views.py
+++ inventory/views.py
@@ -23,7 +23,7 @@ from django import forms
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'category', 'stock', 'price','image']
+        fields = ['name', 'category', 'stock', 'price','image','supplier_name','supplier_contact']
 @staff_member_required
 def add_product(request):
     if request.method == 'POST':
