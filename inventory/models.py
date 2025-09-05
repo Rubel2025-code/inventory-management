2fe2cb40cfe2faee2b4ff2fed543796071148f0b
Rubel <miahrubel768.cse@gmail.com>
2025-09-05 15:22:05 +0600

    add supplier

--- inventory/models.py
+++ inventory/models.py
@@ -8,5 +8,9 @@ class Product(models.Model):
     added_on = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
 
+    # ✅ New fields (admin only)
+    supplier_name = models.CharField(max_length=100, blank=True, null=True)
+    supplier_contact = models.CharField(max_length=50, blank=True, null=True)
+
     def __str__(self):
         return self.name
