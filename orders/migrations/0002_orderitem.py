5339175350f1dbbb4247936861bbd0f4d404f9cb
Rubel <miahrubel768.cse@gmail.com>
2025-09-07 01:19:41 +0600

    fix order

---
+++ orders/migrations/0002_orderitem.py
@@ -0,0 +1,22 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orders', '0001_initial'),
+        ('inventory', '0002_alter_cart_user_alter_cartitem_quantity_and_more'),  # adjust if your last inventory migration is different
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='OrderItem',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('quantity', models.PositiveIntegerField()),
+                ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='order_items', to='orders.order')),
+                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='inventory.product')),
+            ],
+        ),
+    ]
